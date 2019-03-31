d547ec4fa25141b71b379d712d1fbfec4bca8947
sakine218 <snishibayashi218@gmail.com>
2019-03-31 17:44:35 +0900

    Initial Commit

---
+++ PhotoMasterTests/PhotoMasterTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoMasterTests.swift
+//  PhotoMasterTests
+//
+//  Created by Sakine Nishibayashi on 2019/03/31.
+//  Copyright © 2019 Sakine Nishibayashi. All rights reserved.
+//
+
+import XCTest
+@testable import PhotoMaster
+
+class PhotoMasterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
